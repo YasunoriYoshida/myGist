98dd3839c29d853186c843487c3820593d4abefc
Yasunori Yoshida <yoshida.yasunori@po.ntts.co.jp>
2013-01-26 12:19:29 +0900

    comment add (test)

--- windows/shutdownBlock/shutdownBlock/Form1.cs
+++ windows/shutdownBlock/shutdownBlock/Form1.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 
+/// <summary>
+/// Shutdown Blcok Sample
+/// </summary>
 namespace shutdownBlock
 {
     public partial class Form1 : Form
